7812a0d481440ae40dadfe37f6563de2675da171
ashutoshdubey133 <ashutoshdubey133@gmail.com>
2022-04-02 13:36:43 +0530

    Added PlaceHolder For CartItems using Js

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 function addItem(){
   const cartList = document.getElementById("cartList");
-  const para = document.createElement("p");
+  const itembg = document.createElement("div");
+  itembg.style.cssText = `
+  background-color: #ffc4ff;
+  width: 58vw;
+  height: 8vh;
+  margin-left: 1vw;
+  margin-top: 2vh;
+  margin-right: 7vw;
+  border-radius: 3vh;
+  `;
 
-  const node = document.createTextNode("Hello from js");
-
-  para.appendChild(node);
-  cartList.appendChild(para);
+  cartList.appendChild(itembg);
 }
